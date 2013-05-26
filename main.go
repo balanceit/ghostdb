77bf639fbc3471360b9edf7e80929d7750ee0fd6
David Mitchell <david.a.mitchell@gmail.com>
2013-05-26 22:30:15 +1200

    removed all from main

--- main.go
+++ main.go
@@ -1,14 +1,8 @@
 // ghostdb
 package main
 
-import (
-	"fmt"
-	"github.com/balanceit/ghostdb/cli"
-)
+// see the tests
+import ()
 
 func main() {
-	fmt.Printf("calling Get() = %v\n", cli.Get())
-	fmt.Printf("calling Put() = %v\n", cli.Put())
-	fmt.Printf("calling Post() = %v\n", cli.Post())
-	fmt.Printf("calling Delete() = %v\n", cli.Delete())
 }
